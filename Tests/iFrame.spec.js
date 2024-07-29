5547c4aa083cb708794a886a256d591650622dd3
atul patil <atulp441@gmail.com>
2024-07-29 18:26:57 +0530

    add iframe handle files

---
+++ Tests/iFrame.spec.js
@@ -0,0 +1,16 @@
+const {test,expect} = require('@playwright/test')
+
+test('Handle Iframe', async ({page}) => {
+
+    // Navigate to a webpage.
+    await page.goto('https://rahulshettyacademy.com/AutomationPractice/');
+
+    // Switch to the iframe
+    const iframe1 = page.frameLocator('#courses-iframe');
+    await iframe1.locator("li a[href*='lifetime-access']:visible").click();
+
+
+
+
+
+});
